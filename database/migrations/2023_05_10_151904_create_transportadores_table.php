e846c5b1a1e818661b51396891956b5ae0845aab
Matheus <matheuspsilva29@gmail.com>
2023-05-10 22:07:09 -0300

    Ajuste propriedades migration

--- database/migrations/2023_05_10_151904_create_transportadores_table.php
+++ database/migrations/2023_05_10_151904_create_transportadores_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('transportadores', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->string('cnpj');
+            $table->string('cnpj')->unique();
             $table->timestamps();
         });
     }
--- database/migrations/2023_05_10_170622_create_destinatarios_table.php
+++ database/migrations/2023_05_10_170622_create_destinatarios_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('destinatarios', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
-            $table->string('cod');
+            $table->string('cod')->unique();
             $table->timestamps();
         });
     }
